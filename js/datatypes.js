17a40a8d7903ba01ecf447328fe1db1726333081
Alex Velazquez72 <2302040@utrivieramaya.edu.mx>
2024-10-23 07:49:58 -0500

    Pirmer commit

---
+++ js/datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof 'Pedro Pony');
+console.log(typeof 9);
+console.log(typeof true);
+console.log(typeof null)
---
+++ js/variable.js
@@ -0,0 +1,32 @@
+let nombre = 'pedro Pony';
+let erepedro = true;
+let edad;
+let direccion = null;
+
+
+let materia1;
+let materia2;
+let materia3;
+let materia4;
+
+let profesor = 'prof. Pino';
+let profesor2 = 'Prof. Perla';
+let profesor3 = 'Prof. Geovani';
+let profesor4 = 'Prof. Albino';
+
+let myName = 'Pedro';
+let myLastName = 'Pony';
+let isMarried = false;
+let country = 'Mexico'
+let age = 9
+
+let myName1, myLastName1, isMarried1, country1, age1;
+
+myName1 = 'Adrian';
+myName1 = 'Cecilia';
+myName1 = 'Sebas';
+
+let myAge = 19;
+let yourAge = 25;
+
+console.log('Mi edad es ' + myAge + ' y tu edad es ' + yourAge);
